5b3bd77cea30da832fd0691268eb7d83737325b5
Nick Sullivan <nick.dave.sullivan@gmail.com>
2026-03-14 17:09:24 +1030

    better view for mobile

--- src/deck-showcase.tsx
+++ src/deck-showcase.tsx
@@ -4,6 +4,7 @@ import { BurgerMenu } from "./components/BurgerMenu/BurgerMenu";
 import "./global.css";
 import { DeckShowcase } from "./pages/DeckShowcase/DeckShowcase";
 
+// Inspired by https://deck-passport.vercel.app/deck/new
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BurgerMenu />
